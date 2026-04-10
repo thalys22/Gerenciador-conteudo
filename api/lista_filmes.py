c0cc3f3c33ea404da5f36f95f9b528e8ac74da27
Thalys <thalys_santos@hotmail.com>
2026-04-10 16:34:57 -0300

    Corrigindo chaves de Api

--- api/lista_filmes.py
+++ api/lista_filmes.py
@@ -1,16 +1,17 @@
 import urllib.request
 import json
+import os
 
 def resultado_filmes(tipo):
+  api_key = os.getenv('TMDB_API_KEY')
   if tipo == 'Populares':
-    url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=12c55a4f43166db5995b9d0f68377f50'
+    url = f'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={api_key}'
   elif tipo == "Animação":
-    url = 'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=12c55a4f43166db5995b9d0f68377f50'
+    url = f'https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key={api_key}'
   elif tipo == "2010":
-    url = 'https://api.themoviedb.org/3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&api_key=12c55a4f43166db5995b9d0f68377f50'
+    url = f'https://api.themoviedb.org/3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&api_key={api_key}'
   
   resposta = urllib.request.urlopen(url)
   dados = resposta.read()
   dados_json = json.loads(dados)
   return dados_json['results']
-  #print(dados_json['results'])
